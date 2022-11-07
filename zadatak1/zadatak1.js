53220783a4650d30fe485a9fcc2e2700cb6ba0dd
Eman <eduvnjak1@etf.unsa.ba>
2022-11-07 12:51:21 +0100

    Ispravljen zadatak 1

--- zadatak1/zadatak1.js
+++ zadatak1/zadatak1.js
@@ -1,9 +1,8 @@
 
-var divs = document.getElementsByClassName("zatvoren_meni");
+var divs = document.getElementsByClassName("meni");
 for (let index = 0; index < divs.length; index++) {
     const element = divs[index];
-    element.addEventListener("click", function () {
-        this.classList.toggle("otvoren_meni");
-        this.classList.toggle("zatvoren_meni");
+    element.getElementsByClassName("godina")[0].addEventListener("click", function () {
+        element.classList.toggle("meni");
     });
 }
