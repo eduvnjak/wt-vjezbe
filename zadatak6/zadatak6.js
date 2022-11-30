62e0a9ef73ff0ea9507effca58b6d820af5c96e9
Eman <eduvnjak1@etf.unsa.ba>
2022-11-30 01:25:49 +0100

    Zadatak 6 - validacija nakon gubitka fokusa

--- zadatak6/zadatak6.js
+++ zadatak6/zadatak6.js
@@ -1,73 +1,117 @@
-function provjeriFormu() {
-    var greska = document.getElementById('greska');
-    // ili document.body.children[0]
-    greska.innerHTML = ""; // ocistimo prethodne greske
-    var formaIspravna = true;
+function oznaciUnosValidan(element) {
+    element.classList.remove("nevalidan");
+    element.classList.add("validan");
+}
 
-    var forma = document.getElementById('mfid');
+function oznaciUnosNevalidan(element) {
+    element.classList.remove("validan");
+    element.classList.add("nevalidan");
+}
+const greska = document.getElementById('greska');
+// ili document.body.children[0]
+const forma = document.getElementById('mfid');
+
+function validacijaImena() {
     if (forma.ime.value.length > 10 || forma.ime.value.length < 3) {
-        forma.ime.classList.remove("validan");
-        forma.ime.classList.add("nevalidan");
-        greska.innerHTML += "Predugo/prekratko ime<br>";
-        formaIspravna = false;
+        oznaciUnosNevalidan(forma.ime);
+        if (greska.innerHTML.indexOf("Predugo/prekratko ime<br>") == -1) {
+            greska.innerHTML += "Predugo/prekratko ime<br>";
+        }
+        return false;
     } else {
-        forma.ime.classList.remove("nevalidan");
-        forma.ime.classList.add("validan");
+        oznaciUnosValidan(forma.ime);
+        greska.innerHTML = greska.innerHTML.replace("Predugo/prekratko ime<br>", "");
+        return true;
     }
+}
 
+function validacijaPrezimena() {
     if (forma.prezime.value.trim() == "") {
-        greska.innerHTML += "Prezime nije unseseno<br>";
-        formaIspravna = false;
-        forma.prezime.classList.remove("validan");
-        forma.prezime.classList.add("nevalidan");
+        if (greska.innerHTML.indexOf("Prezime nije unseseno<br>") == -1) {
+            greska.innerHTML += "Prezime nije unseseno<br>";
+        }
+        oznaciUnosNevalidan(forma.prezime);
+        return false;
     } else {
-        forma.prezime.classList.remove("nevalidan");
-        forma.prezime.classList.add("validan");
+        oznaciUnosValidan(forma.prezime);
+        greska.innerHTML = greska.innerHTML.replace("Prezime nije unseseno<br>", "");
+        return true;
     }
+}
 
-    // Ovaj regex ukucajte u regex101.com za tumacenje:
+function validacijaTelefona() {
     var telefonRegEx = /^\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{3})$/;
     if (!telefonRegEx.test(forma['telefon'].value)) {
-        greska.innerHTML += "Telefon format: (061)-123-345 ili 061-123-456 ili 061123456<br>";
-        formaIspravna = false;
-        forma['telefon'].classList.add("nevalidan");
-        forma['telefon'].classList.remove("validan");
+        if (greska.innerHTML.indexOf("Telefon format: (061)-123-345 ili 061-123-456 ili 061123456<br>") == -1) {
+            greska.innerHTML += "Telefon format: (061)-123-345 ili 061-123-456 ili 061123456<br>";
+        }
+        oznaciUnosNevalidan(forma['telefon']);
+        return false;
     } else {
-        forma['telefon'].classList.remove("nevalidan");
-        forma['telefon'].classList.add("validan");
+        oznaciUnosValidan(forma['telefon']);
+        greska.innerHTML = greska.innerHTML.replace("Telefon format: (061)-123-345 ili 061-123-456 ili 061123456<br>", "");
+        return true;
     }
+}
 
+function validacijaEmaila() {
     var emailRegEx = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/g;
     if (!emailRegEx.test(forma['email'].value)) {
-        greska.innerHTML += "Email nevalidan!<br>";
-        formaIspravna = false;
-        forma['email'].classList.add("nevalidan");
-        forma['email'].classList.remove("validan");
+        if (greska.innerHTML.indexOf("Email nevalidan!<br>") == -1) {
+            greska.innerHTML += "Email nevalidan!<br>";
+        }
+        oznaciUnosNevalidan(forma['email']);
+        return false;
     } else {
-        forma['email'].classList.remove("nevalidan");
-        forma['email'].classList.add("validan");
+        oznaciUnosValidan(forma['email']);
+        greska.innerHTML = greska.innerHTML.replace("Email nevalidan!<br>", "");
+        return true;
     }
+}
 
+function validacijaPassworda() {
     if (forma.password.value == "") {
-        greska.innerHTML += "Password nije unesen<br>";
-        formaIspravna = false;
-        forma['password'].classList.add("nevalidan");
-        forma['password'].classList.remove("validan");
+        if (greska.innerHTML.indexOf("Password nije unesen<br>") == -1) {
+            greska.innerHTML += "Password nije unesen<br>";
+        }
+        oznaciUnosNevalidan(forma['password']);
+        return false;
     } else {
-        forma['password'].classList.remove("nevalidan");
-        forma['password'].classList.add("validan");
+        oznaciUnosValidan(forma['password']);
+        greska.innerHTML = greska.innerHTML.replace("Password nije unesen<br>", "");
+        return true;
     }
+}
 
+function validacijaPotvrdiPassword() {
     const potvrdiPassword = document.getElementById("potvrdi_password");
     if (forma.password.value != potvrdiPassword.value) {
-        greska.innerHTML += "Passwordi se ne podudaraju<br>";
-        formaIspravna = false;
-        potvrdiPassword.classList.add("nevalidan");
-        potvrdiPassword.classList.remove("validan");
+        if (greska.innerHTML.indexOf("Passwordi se ne podudaraju<br>") == -1) {
+            greska.innerHTML += "Passwordi se ne podudaraju<br>";
+        }
+        oznaciUnosNevalidan(potvrdiPassword);
+        return false;
     } else {
-        potvrdiPassword.classList.remove("nevalidan");
-        potvrdiPassword.classList.add("validan");
+        oznaciUnosValidan(potvrdiPassword);
+        greska.innerHTML = greska.innerHTML.replace("Passwordi se ne podudaraju<br>", "");
+        return true;
     }
+
+}
+forma.ime.addEventListener("blur", validacijaImena);
+forma.prezime.addEventListener("blur", validacijaPrezimena);
+forma.telefon.addEventListener("blur", validacijaTelefona);
+forma.email.addEventListener("blur", validacijaEmaila);
+forma.password.addEventListener("blur", validacijaPassworda);
+document.getElementById("potvrdi_password").addEventListener("blur", validacijaPotvrdiPassword);
+
+function provjeriFormu() {
+    greska.innerHTML = "";
+    var formaIspravna = true;
+
+    formaIspravna = validacijaImena() &
+        validacijaPrezimena() & validacijaTelefona() &
+        validacijaEmaila() & validacijaPassworda() & validacijaPotvrdiPassword();
 
     if (!forma.sretan.checked) {
         greska.innerHTML += "Moraš biti sretan!<br>";
@@ -77,10 +121,9 @@ function provjeriFormu() {
     var sel = forma.godina.options[forma.godina.selectedIndex].text;
     // vazan je i .value
     if (sel !== "Druga") {
-        greska.innerHTML += "Moraš biti druga godina!";
+        greska.innerHTML += "Moraš biti druga godina!<br>";
         formaIspravna = false;
     }
-    console.log(formaIspravna);
     return formaIspravna;
 }
 
