af74bdd9347cab09c9676a4c3ef946f47d58962b
Eman <eduvnjak1@etf.unsa.ba>
2022-11-30 01:29:50 +0100

    Dodan drugi regex (sa vjezbi) za treci zadatak

--- zadatak3/zadatak3.js
+++ zadatak3/zadatak3.js
@@ -1,5 +1,6 @@
 function zamijeni() {
     var kod = textArea.value;
+    var regex_drugi = /\<.+?\>/g; //regex asistentov sa vjezbi
     kod = kod.replaceAll(/\s*?<.+?>\s*?/g,"").replaceAll(/&amp;/g,"&").replaceAll(/&nbsp;/g," ").replaceAll(/&quot;/g,'"');
     textArea.value = kod;
 }
